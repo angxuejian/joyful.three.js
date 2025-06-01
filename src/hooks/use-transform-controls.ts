83ebc9a9390f23219857d5ca2973036911a8d17a
angxuejian <xuejian.ang@gmail.com>
2025-06-01 12:48:52 +0800

    feat: 新增transform controls

---
+++ src/hooks/use-transform-controls.ts
@@ -0,0 +1,12 @@
+// import * as THREEo9 from 'three'
+// import { TransformControls } from 'three/addons/controls/TransformControls.js';
+// import { onMounted } from 'vue'
+
+// export function useTransformControls({
+//   camera,
+//   domElement
+// }) {
+
+// }
+
+
